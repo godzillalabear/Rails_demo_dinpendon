6bc25c30b868eebd24d3d0be07f9ceb1a76e9712
Yushan Cheng <b10432020@gapps.ntust.edu.tw>
2020-03-26 18:06:35 +0800

    init commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,14 @@
+# welcome_controller.rb
+
+class WelcomeController < ApplicationController
+  
+  def index
+    # render html: "hi"
+    # /app/views/welcome/index.html.erb
+  end
+  def login
+  end
+  def create
+  end
+
+end
