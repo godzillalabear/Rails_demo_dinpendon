b222f99b86f0831ffb927c40903af6474be9a485
Yushan Cheng <b10432020@gapps.ntust.edu.tw>
2020-04-01 18:34:26 +0800

    email format

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, 
+                    uniqueness: true, 
+                    format: { with: /[\w-]+@([\w-]+\.)+[\w-]+/}
   validates :password,  presence: true, 
                         confirmation: true, 
                         length: {minimum: 4}
